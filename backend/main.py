30f90010f917521ae65c0f02c74893469a20a3d2
SujalKamate <sujal.kamate@nmiet.edu.in>
2026-04-19 17:43:19 +0530

    Added search API

--- backend/main.py
+++ backend/main.py
@@ -19,3 +19,23 @@ def get_items():
     conn.close()
 
     return {"items": rows}
+@app.get("/search/{item_name}")
+def search_item(item_name: str):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    query = """
+    SELECT i.item_id, i.name, c.container_id, c.zone_id
+    FROM Items i
+    LEFT JOIN Placements p ON i.item_id = p.item_id
+    LEFT JOIN Containers c ON p.container_id = c.container_id
+    WHERE LOWER(i.name) LIKE :name
+    """
+
+    cursor.execute(query, {"name": f"%{item_name.lower()}%"})
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return {"results": rows}
